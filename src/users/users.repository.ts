cc0814a2441cd0d12797c19c1b08028a8b17cf99
programmingwithnavilla <navidbarsalari@gmail.com>
2025-06-27 00:01:16 +0330

    findOne rename to findUnique

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -27,7 +27,7 @@ export class UsersRepository extends BaseRepository<any> {
     await this.prisma.user.delete({ id: id });
     await this.incrementVersion(id);
   }
-  async findOne(id: number) {
+  async findUnique(id: number) {
     const user = await this.prisma.user.findUnique({ where: { id: id } });
     return user;
   }
