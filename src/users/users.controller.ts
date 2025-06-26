ca26d0f20ecda1c2558d692a818dead36e14a9c7
programmingwithnavilla <navidbarsalari@gmail.com>
2025-06-27 02:10:43 +0330

    add controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,7 +4,9 @@ import {
   Controller,
   Get,
   Post,
-  Query,
+  Put,
+  Delete,
+  Param,
 } from '@nestjs/common';
 import { CreateUserSchema } from './dto/create-user.dto';
 import { UsersService } from './users.service';
@@ -12,6 +14,7 @@ import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
   constructor(private userService: UsersService) {}
+
   @Post()
   async create(@Body() body: any) {
     const parsed = CreateUserSchema.safeParse(body);
@@ -27,7 +30,21 @@ export class UsersController {
   }
 
   @Get(':id')
-  async findOne(@Query() id: string) {
+  async findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() body: any) {
+    const parsed = CreateUserSchema.safeParse(body);
+    if (!parsed.success) {
+      throw new BadRequestException(parsed.error.format());
+    }
+    return this.userService.update(+id, parsed.data);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.userService.delete(+id);
   }
 }
