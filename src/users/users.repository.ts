77468ea56d490a4ae1f3e9ad37529d1c74cce23f
programmingwithnavilla <navidbarsalari@gmail.com>
2025-06-26 23:48:06 +0330

    add create in user repository

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -7,4 +7,9 @@ export class UsersRepository extends BaseRepository<any> {
   constructor(private readonly prisma: PrismaClient) {
     super(prisma.user);
   }
+
+  async create(data: any) {
+    const user = await this.prisma.user.create({ data: data });
+    return user;
+  }
 }
