e4a6bc280ec904250a00163c9b9acbc9fc2f2b9f
programmingwithnavilla <navidbarsalari@gmail.com>
2025-06-27 01:46:42 +0330

    feat(user): integrate Prisma User model with UnitOfWork repository

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,51 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
-import { BaseRepository } from 'src/common/base.repository';
+import { User } from '@prisma/client';
+import { PrismaUnitOfWork } from 'src/database/prisma.unit-of-work';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { RedisService } from 'src/redis/redis.service';
 
 @Injectable()
-export class UsersRepository extends BaseRepository<any> {
-  constructor(private readonly prisma: PrismaClient) {
-    super(prisma.user);
-  }
-
-  async create(data: any) {
-    const user = await this.prisma.user.create({ data: data });
-    await this.incrementVersion(user.id);
-    return user;
-  }
-
-  async update(id: number, data: any) {
-    const user = await this.prisma.user.update({
-      where: { id: id },
-      data: data,
-    });
-    await this.incrementVersion(id);
-    return user;
-  }
-
-  async delete(id: number) {
-    await this.prisma.user.delete({ id: id });
-    await this.incrementVersion(id);
-  }
-  async findOne(id: number) {
-    const user = await this.prisma.user.findUnique({ where: { id: id } });
-    return user;
-  }
-
-  async findAll() {
-    const users = await this.prisma.user.findMany({});
-    return users;
-  }
-
-  private async incrementVersion(id: number) {
-    console.log('id', id);
-  }
-  private async setVersion(userId: number, version: number) {
-    console.log(userId, version);
-    // await this.
-  }
-
-  private resetVersion(userId: number) {
-    console.log('userId', userId);
+export class UserRepository extends PrismaUnitOfWork<
+  User,
+  PrismaService['user']
+> {
+  constructor(prisma: PrismaService, redis: RedisService) {
+    super(prisma.user, redis, 'user');
   }
 }
