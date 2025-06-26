624e53282c7b8f33a5ac1d70214e9ac73a3a5d08
programmingwithnavilla <navidbarsalari@gmail.com>
2025-06-26 14:30:50 +0330

    user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateUserSchema } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private userService: UsersService) {}
+  @Post()
+  async create(@Body() body: any) {
+    const parsed = CreateUserSchema.safeParse(body);
+    if (!parsed.success) {
+      throw new BadRequestException(parsed.error.format());
+    }
+    return this.userService.create(parsed.data);
+  }
+
+  @Get()
+  async findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Query() id: string) {
+    return this.userService.findOne(+id);
+  }
+}
