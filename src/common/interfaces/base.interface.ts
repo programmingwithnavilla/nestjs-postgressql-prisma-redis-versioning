3da881ec2d27ddc82b92247f18b315724f8cbc53
programmingwithnavilla <navidbarsalari@gmail.com>
2025-06-27 06:09:45 +0330

    add base interface

--- src/common/interfaces/base.interface.ts
+++ src/common/interfaces/base.interface.ts
@@ -2,5 +2,24 @@ export interface QueryParams {
   page?: number;
   size?: number;
   sort?: string;
-  filter?: Record<string, unknown>;
+  filters?: Record<string, unknown>;
 }
+
+export type Primitive = string | number | boolean | null;
+export type FilterOperator =
+  | 'eq'
+  | 'lt'
+  | 'lte'
+  | 'gt'
+  | 'gte'
+  | 'contains'
+  | 'startsWith'
+  | 'endsWith';
+
+export type FilterInput<T> = {
+  [K in keyof T]?:
+    | T[K]
+    | {
+        [P in FilterOperator]?: T[K];
+      };
+};
